82581cecc01c58a6b4d9acd95c5a27f0dd9156ba
Rich Harris <richard.a.harris@gmail.com>
2013-02-27 12:01:50 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# s3-batch
+
+Utility for batch uploading files to S3. This is a rough-and-ready script developed for personal use, YMMV. It assumes you are using the AWS Node SDK - refer to the docs at <http://aws.amazon.com/sdkfornodejs/>
+
+## Usage
+
+```js
+var S3Batch, batch;
+
+S3Batch = require( 's3-batch' );
+
+// you need to have your own s3 instance set up
+batch = new S3Batch( s3 ); // optionally you can provide default parameters as a second argument, e.g. { Bucket: 'my-bucket' }
+```
+
+## Instance methods
+### `batch.add( key, data[, params] );`
+Adds a new file to the queue. `params` extend the defaults, and are as per the s3.putObject parameters (http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3/Client.html#putObject-property)
+
+### `batch.on( eventName, callback )`
+Adds an event callback (see **Instance events** below)
+
+### `batch.start();`
+Starts uploading
+
+
+## Instance properties
+### `batch.s3`
+The S3 instance we initialised with
+
+### `batch.total.files`
+The number of files in the queue
+
+### `batch.total.bytes`
+The number of bytes in the queue
+
+### `batch.completed.files`
+The number of files successfully uploaded
+
+### `batch.completed.bytes`
+The number of bytes successfully uploaded
+
+
+## Instance events
+Below, `file` as a callback argument refers to an object containing `key`, `data` and `params` objects as originally passed in to `batch.add`
+
+### `success`
+Fired when a file is successfully uploaded. Callbacks receive `file`
+
+### `progress`
+Fired when a file is successfully uploaded. Callbacks receive a number between 0 and 1, indicating batch progress. The number of bytes or files completed can be accessed using the instance properties listed above
+
+### `error`
+Fired when a file fails to upload. Callbacks receive `file`
+
+### `complete`
+Fired when all files have successfully uploaded (if any uploads result in an error, this will never fire)
+
---
+++ index.js
@@ -0,0 +1,115 @@
+(function () {
+
+	'use strict';
+
+	var S3Batch, extend;
+
+	extend = require( 'extend' );
+
+	S3Batch = function ( s3, defaults ) {
+		this.s3 = s3;
+		this._defaults = defaults || {};
+
+		this._queue = [];
+
+		this.total = {
+			files: 0,
+			bytes: 0
+		};
+
+		this.completed = {
+			files: 0,
+			bytes: 0
+		};
+
+		this.subscribers = {};
+	};
+
+	S3Batch.prototype = {
+		add: function ( key, data, params ) {
+			var file;
+
+			file = {
+				key: key,
+				data: data,
+				params: params || {}
+			};
+
+			this._queue.push( file );
+
+			this.total.files += 1;
+			this.total.bytes += data.length;
+
+			this.fire( 'add', file );
+		},
+
+		start: function () {
+			var self, next, putObjectRequest, inProgress, total, completed;
+
+			self = this;
+			
+			self.inProgress = [];
+			
+			this._queue.forEach( function ( file, i ) {
+				
+				var params;
+
+				self.inProgress.push( file );
+
+				self.fire( 'start', file );
+
+				params = extend({}, self._defaults, file.params, {
+					Key: file.key,
+					Body: file.data
+				});
+
+				putObjectRequest = self.s3.client.putObject( params );
+				
+				putObjectRequest.on( 'error', function () {
+					self.fire( 'error', file );
+				});
+
+				putObjectRequest.on( 'success', function () {
+					
+					self.completed.files += 1;
+					self.completed.bytes += file.data.length;
+
+					self.fire( 'success', file );
+
+					// remove from inProgress
+					inProgress.splice( self.inProgress.indexOf( params ), 1 );
+
+					// fire progress event
+					self.fire( 'progress', self.completed.bytes / self.total.bytes );
+
+					// if there are no more, fire complete event
+					if ( !inProgress.length ) {
+						self.fire( 'complete' );
+					}
+				});
+
+				putObjectRequest.send();
+			});
+		},
+
+		on: function ( eventName, callback ) {
+			if ( !this.subscribers[ eventName ] ) {
+				this.subscribers[ eventName ] = [];
+			}
+
+			this.subscribers[ eventName ].push( callback );
+		},
+
+		fire: function ( eventName ) {
+			var args = Array.prototype.slice.call( arguments, 1 ),
+				subscribers = this.subscribers[ eventName ] || [];
+
+			subscribers.forEach( function ( subscriber ) {
+				subscriber.apply( null, args );
+			});
+		}
+	};
+
+	module.exports = S3Batch;
+
+}());
